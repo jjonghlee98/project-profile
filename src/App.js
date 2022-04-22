b7de48ee845f3572cb00cd8acdbb51aae5b15b9c
Jonghwan Lee <jjonghlee98@gmail.com>
2022-04-22 23:45:10 +0900

    react study first-day

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+// import ExpenseItem from "./components/ExpenseItem";
+import Expenses from './components/Expenses';
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Jonghwan",
+      date: new Date(2022, 4, 22),
+      amount: 32.99,
+    },
+    {
+      id: "e2",
+      title: "Junghwan",
+      date: new Date(2022, 4, 23),
+      amount: 52.99,
+    },
+    {
+      id: "e3",
+      title: "Hee",
+      date: new Date(2022, 4, 24),
+      amount: 15.99,
+    },
+    {
+      id: "e4",
+      title: "Hae",
+      date: new Date(2022, 4, 25),
+      amount: 99.99,
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses}/>
     </div>
   );
 }
-
 export default App;
